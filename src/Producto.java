62e93e6cf3c73aff1e73f1c8a233c07e3747be28
Carlos Andres Castrillon Oviedo <carlos.castrillonov@campusucc.edu.co>
2025-04-06 16:39:12 -0500

    cracion de atributos de la clase producto

---
+++ src/Producto.java
@@ -0,0 +1,13 @@
+public class Producto {
+    //atributos
+    private String nombre;
+    private String descripcion;
+    private String categoria;
+    private String codigo;
+    private int catidad;
+    private double precio;
+
+
+
+
+}
