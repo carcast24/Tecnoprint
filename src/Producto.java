f571b50496d771b1787df4f0bf8f9534da138b28
Carlos Andres Castrillon Oviedo <carlos.castrillonov@campusucc.edu.co>
2025-04-06 16:48:20 -0500

    cracion de constructor de la clase producto

--- src/Producto.java
+++ src/Producto.java
@@ -4,10 +4,19 @@ public class Producto {
     private String descripcion;
     private String categoria;
     private String codigo;
-    private int catidad;
+    private int cantidad;
     private double precio;
 
+    // constructor
 
+    public Producto(String nombre, String descripcion, String categoria, String codigo, int cantidad, double precio){
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.categoria = categoria;
+        this.codigo = codigo;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
 
 
 }
