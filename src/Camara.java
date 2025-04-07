706b96b3eec99db417bb036268463d2fed1d69ce
Carlos Andrés Castrillón <carlos.castrillonov@campusucc.edu.co>
2025-04-07 17:39:17 +0000

    cracion de clase camara, con herencia de la clase Producto, creacion de constructor, y metodos getter

---
+++ src/Camara.java
@@ -0,0 +1,25 @@
+public class Camara extends Producto {
+    // atributos de clase 
+    private String marca;
+    private String modelo;
+
+    // constructor de clase con herencia de superclase 
+
+    public Camara (String nombre, String descripcion, String categoria, String codigo, int cantidad, double precio, String marca, String modelo){
+        super (nombre, descripcion, categoria, codigo, cantidad,precio);
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    // getter
+
+    public String getMarca () {
+        return marca;
+    }
+
+    public String getModelo () {
+        return modelo;
+    }
+
+
+}
