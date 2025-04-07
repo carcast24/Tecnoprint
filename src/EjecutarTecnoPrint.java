435764f50b10a0d6e7caaa468610e0d76770d411
Carlos Andres Castrillon Oviedo <carlos.castrillonov@campusucc.edu.co>
2025-04-07 18:04:24 -0500

    creacion de EjecutarTecnoPrint y su primer cliente  jhon

---
+++ src/EjecutarTecnoPrint.java
@@ -0,0 +1,10 @@
+public class EjecutarTecnoPrint {
+    public static void main (String [] args){
+
+        // creacion de clientes
+
+        Cliente [] baseDeDatos1 = new Cliente[2];
+        baseDeDatos1 [0] = new Cliente("Jhon","300 841 54 54","Calle 11 # 35 20","jhoncano@campusucc.edu.co","45955875");
+
+    }
+}
