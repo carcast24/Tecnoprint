359db92e72f22873d01eb46b3c0aa9837726e8ee
Carlos Andres Castrillon Oviedo <carlos.castrillonov@campusucc.edu.co>
2025-04-07 21:53:27 -0500

    le agregue saltos de linea a la descripcion

--- src/EjecutarTecnoPrint.java
+++ src/EjecutarTecnoPrint.java
@@ -14,5 +14,12 @@ public class EjecutarTecnoPrint {
         //actualizar cliente
         baseDeDatos1[0].actualizarCliente("Jhon Canon","300 841 54 54","Cra 35 # 35 20","jhoncano@campusucc.edu.co","45955875");
         System.out.println(baseDeDatos1[0]);
+
+        // alimentamos el inventario de camaras
+
+        Camara objCamaraCanon = new Camara("Eos Rebel","Cámara SLR ideal para principiantes, \ncon sensor CMOS, procesador DIGIC 4, pantalla LCD, \nISO ampliado y funciones creativas","Digital","001",5,1900000.36,"Canon","EOS Rebel T5");
+        System.out.println(objCamaraCanon);
+
+
     }
 }
