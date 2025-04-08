f0d6d2047dc39bd4d2f5c256c4585f4a9fa4de72
Carlos Andres Castrillon Oviedo <carlos.castrillonov@campusucc.edu.co>
2025-04-07 21:24:25 -0500

    cree arreglo de clientes, use el metodo de imprimir la informacion (toString) y use el metodo de actualizar cliente

--- src/EjecutarTecnoPrint.java
+++ src/EjecutarTecnoPrint.java
@@ -5,6 +5,16 @@ public class EjecutarTecnoPrint {
 
         Cliente [] baseDeDatos1 = new Cliente[2];
         baseDeDatos1 [0] = new Cliente("Jhon","300 841 54 54","Calle 11 # 35 20","jhoncano@campusucc.edu.co","45955875");
+        baseDeDatos1 [1] = new Cliente("Javier","305 295 60 65","cra 22A # 65 69","javier.rosero@campusucc.edu.co","6592865");
+
+        // imprimo la informacion
+        System.out.println(baseDeDatos1[0]);
+        System.out.println(baseDeDatos1[1]);
+
+        //actualizar cliente
+        baseDeDatos1[0].actualizarCliente("Jhon Canon","300 841 54 54","Cra 35 # 35 20","jhoncano@campusucc.edu.co","45955875");
+        System.out.println(baseDeDatos1[0]);
+
 
     }
 }
